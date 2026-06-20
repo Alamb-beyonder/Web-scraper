918422f72069fd8a3c0438e05098c81443ffee46
Alamb-beyonder <andy.lam93340@gmail.com>
2026-06-20 15:30:31 +1000

    Selenium to extract title + score

--- main.py
+++ main.py
@@ -1,13 +1,31 @@
 from bs4 import BeautifulSoup
+from selenium import webdriver
 import requests
 
-url = "https://www.metacritic.com/game/sawi-the-voidbuster/"
+
+driver = webdriver.Chrome()
+url = "https://www.metacritic.com/game/the-witcher-3-wild-hunt/"
+driver.get(url)
+html = driver.page_source
+soup = BeautifulSoup(html, "html.parser")
+wrappers = soup.find_all(attrs={"data-testid": "global-score-wrapper"})
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/149.0.0.0 Safari/537.36"
 }
 
-response = requests.get(url, headers=headers)
+scores = {}
 
-content = response.text
-index = content.find("1290")
-print(content[index-100:index+300])
+# response = requests.get(url, headers=headers)
+# content = response.text
+# index = content.find("1290")
+
+for wrapper in wrappers:
+    header = wrapper.find(attrs={"data-testid": "global-score-header"}).text
+    value = wrapper.find(attrs={"data-testid": "global-score-value"}).text
+    
+    if header not in scores:
+        scores[header] = value
+    
+print(scores)
+
+
